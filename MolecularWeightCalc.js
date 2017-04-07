a984b0252d6837de55b4dd8e942d944cf8ffb599
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-04-07 09:38:45 -0700

    began objects

--- MolecularWeightCalc.js
+++ MolecularWeightCalc.js
@@ -1,7 +1,18 @@
-function getElement() {
-var a = document.getElementById("elements");
-var value = a.options[a.selectedIndex].value;
-document.getElementById("output").innerHTML = value;
+function Element(symbol, aNum){
+    this.symbol = symbol;
+    this.aNum = aNum;
+}
+
+var Ac = new Element("Ac", 10000000);
+var Li = new Element("Li", 3);
+
+
+function getElement(elem) {
+//var msg = document.getElementById("elements").value;
+//var value = a.options[a.selectedIndex].value;
+var msg = elem;
+console.log(msg);
+document.getElementById("atoms").innerHTML = msg;
 }
 
 function getCompound() {
