4d3ade0c4566e9045f8c9540e726ec6938b8f53e
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-04-20 08:04:33 -0700

    started tests for elements

---
+++ tests.js
@@ -0,0 +1,20 @@
+//function percentage() {
+//    currentInput = (currentInput / 100)
+//    displayCurrentInput();
+//}
+//
+// Test for changing the current input to a percentage
+//QUnit.test("Percentage test", function (assert) {
+//    allClear();
+//    addDigit('50');
+//    percentage();
+//    assert.equal(document.getElementById("screen").value, "0.5", "Passed - Expected 0.5");
+//});
+
+// Test for getting an the attributes of an element
+QUnit.test("Get Element test", function (assert) {
+    elements('H');
+    getElement();
+    assert.equal(document.getElementById("screen").value, "H", "Passed - Expected H");
+});
+
